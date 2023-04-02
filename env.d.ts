eabd4c3283a9ae51665bb03256654c2bb6ab5bfb
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2023-04-02 21:51:39 +0200

    Move funny typings

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="vite/client" />
+
+interface FunnyMomentVideo {
+    type: 'video'
+    date: string;
+    title: string;
+    description: string;
+    url: string;
+    thumbnail?: string;
+    source?: string;
+}
+
+interface FunnyMomentImage {
+    type: 'image'
+    date: string;
+    title: string;
+    description: string;
+    url: string;
+    fallback?: string;
+    source?: string;
+}
+
+type FunnyMoment = FunnyMomentVideo | FunnyMomentImage
